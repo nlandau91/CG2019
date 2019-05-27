f5527e64ebf02443352411fbb95ef0b93219956b
nlandau91 <43214457+nlandau91@users.noreply.github.com>
2019-05-27 12:55:17 -0300

    Update index.js

--- Proyecto3/parte1/index.js
+++ Proyecto3/parte1/index.js
@@ -1,5 +1,5 @@
 // 📥 Imports
-import { mat4 } from "/libs/gl-matrix/index.js"
+import { mat4, vec4 } from "/libs/gl-matrix/index.js"
 import { getFileContentsAsText } from "/libs/utils.js"
 import { Program, Material, Geometry, SceneObject, SceneLight, Camera, CameraMouseControls } from "/libs/gl-engine/index.js"
 import { parse } from "/libs/gl-engine/parsers/obj-parser.js"
@@ -13,10 +13,6 @@ async function main() {
     const planoGeometryData      = await parse("/models/plano.obj")
     const esferaGeometryData      = await parse("/models/esfera2.obj")
 
-    const basicVertexShaderSource     = await getFileContentsAsText("/shaders/basic.vert.glsl")
-    const basicFragmentShaderSource   = await getFileContentsAsText("/shaders/basic.frag.glsl")
-    const normalsVertexShaderSource   = await getFileContentsAsText("/shaders/normals.vert.glsl")
-    const normalsFragmentShaderSource = await getFileContentsAsText("/shaders/normals.frag.glsl")
     const diffuseVertexShaderSource   = await getFileContentsAsText("/shaders/diffuse.vert.glsl")
     const diffuseFragmentShaderSource = await getFileContentsAsText("/shaders/diffuse.frag.glsl")
     const phongVertexShaderSource   = await getFileContentsAsText("/shaders/phong.vert.glsl")
@@ -60,20 +56,17 @@ async function main() {
 
     // #️⃣ Programas de shaders disponibles
 
-    const basicProgram   = new Program(gl, basicVertexShaderSource, basicFragmentShaderSource)
-    const normalsProgram = new Program(gl, normalsVertexShaderSource, normalsFragmentShaderSource)
     const diffuseProgram = new Program(gl, diffuseVertexShaderSource, diffuseFragmentShaderSource)
     const phongProgram = new Program(gl, phongVertexShaderSource, phongFragmentShaderSource)
     const phongTProgram = new Program(gl, phongTVertexShaderSource, phongTFragmentShaderSource)
 
     // #️⃣ Creamos materiales combinando programas con distintas propiedades
 
-    const whiteBasicMaterial   = new Material(basicProgram, false, { color: [1, 1, 1] })
-    const normalsMaterial      = new Material(normalsProgram, false)
+
     const whiteDiffuseMaterial = new Material(diffuseProgram, true, { Ka: [0.1, 0.1, 0.1], Kd: [1, 1, 1] })
     const phongMaterial = new Material(phongProgram, true, false, { Ka: [0.1,0.1,0.1], Kd: [0.4,0.4,0.4], Ks: [0.8,0.8,0.8], shininess: 50})
-    const planoMaterial = new Material(phongTProgram, true, true, { shininess: 50})
-    const esferaMaterial = new Material(phongTProgram, true, true, { shininess: 100})
+    const planoMaterial = new Material(phongTProgram, true, true, { texture0: 0, shininess: 50})
+    const esferaMaterial = new Material(phongTProgram, true, true, { texture0: 0, shininess: 100})
     
 
     // #️⃣ Creamos los objetos de la escena
@@ -88,13 +81,13 @@ async function main() {
     const lightColor0 = [1, 1, 1]
     const lightSpotDirection0 = [0,-1,0]
     const lightSpotCutoff0 = -1
-    const light0 = new SceneLight(lightPosition0, lightColor0)
+    const light0 = new SceneLight(lightPosition0, lightColor0, lightSpotDirection0, lightSpotCutoff0)
 
     const ligthPosition1 = [5, -5, -5, 1]
     const lightColor1 = [1, 1, 1]
     const lightSpotDirection1 = [0,-1,0]
     const lightSpotCutoff1 = -1
-    const light1 = new SceneLight(ligthPosition1, lightColor1)
+    const light1 = new SceneLight(ligthPosition1, lightColor1, lightSpotDirection1, lightSpotCutoff1)
 
     const sceneLights = [light0,light1]
 
@@ -142,17 +135,31 @@ async function main() {
                 const value = object.material.properties[name]
                 object.material.program.setUniformValue("material." + name, value)
             }
-            if (object.material.affectedByLight) {
+            if ( object.material.affectedByLight ) {
                 let i = 0
-                for(let light of sceneLights){
-                    
-                    object.material.program.setUniformValue("light"+ i.toString() + ".position",light.position)
-                    object.material.program.setUniformValue("light"+ i.toString() + ".color",light.color)
-                    object.material.program.setUniformValue("light"+ i.toString() + ".spot_direction",light.spot_direction)
-                    object.material.program.setUniformValue("light"+ i.toString() + ".spot_cutoff",light.spot_cutoff)
-                    i++
+                for( let light of sceneLights ) {
+                    //paso la informacion de las luces. La posicion de la luz y la direccion del spot son convertidas a coordenadas del ojo
+                    //si la luz esta asociada a un objeto, se obtiene su posicion y rotacion a partir del objeto
+                    if(light.model != null){
+                        //si la luz esta asociada a un objeto, actualizo su posicion y rotacion
+                        light.position[0] = light.model.position[0]
+                        light.position[1] = light.model.position[1]
+                        light.position[2] = light.model.position[2]
+                        let newSpotDirection = vec4.create()
+                        vec4.transformQuat(newSpotDirection,light.default_spot_direction,light.model.rotQuat)
+                        light.spot_direction = newSpotDirection
+                    }
+                    let lightPosEye = vec4.create();
+                    vec4.transformMat4( lightPosEye, light.position, camera.viewMatrix )
+                    object.material.program.setUniformValue( 'light'+ i.toString() + '.position', lightPosEye )
+                    object.material.program.setUniformValue( 'light'+ i.toString() + '.color', light.color )                                   
+                    let spotDirEye = vec4.create()
+                    vec4.transformMat4( spotDirEye, light.spot_direction, camera.viewMatrix )
+                    object.material.program.setUniformValue( 'light'+ i.toString() + '.spot_direction', spotDirEye )   
+                    object.material.program.setUniformValue( 'light'+ i.toString() + '.spot_cutoff', light.spot_cutoff )
+                    i++                    
                 }
-            }          
+            }                 
 
             // Seteamos info de su geometria
             object.vertexArray.bind()
@@ -160,10 +167,9 @@ async function main() {
             //info de texturas
             if( object.material.textured ) {
                 let i
-                for(i = 0; i < object.texture.length; i++) {                                                                     
-                    gl.activeTexture( gl.TEXTURE0 + i)
-                    gl.bindTexture( gl.TEXTURE_2D, object.texture[i] )
-                    gl.uniform1i( object.material.program.program.samplerUniform, i )
+                for(i = 0; i < object.textures.length; i++) {                                                                     
+                    gl.activeTexture( gl.TEXTURE0 + i )
+                    gl.bindTexture( gl.TEXTURE_2D, object.textures[i] )
                 }
             }
             // Lo dibujamos
